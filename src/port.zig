6e48a6ecdb8f747e07485df559fe15f2f6e37f38
QtumWyn <qtummechanic25@gmail.com>
2026-08-17 11:49:47 -0400

    feat: added new port related file

--- src/port.zig
+++ src/port.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const ExpectedState = enum {
     free,
     listening,
@@ -8,3 +10,20 @@ pub const Spec = struct {
     port: u16,
     expected: ExpectedState,
 };
+
+pub fn isListening(
+    io: std.Io,
+    port_number: u16,
+) bool {
+    var address: std.Io.net.IpAddress = .{
+        .ip4 = .loopback(port_number),
+    };
+
+    var stream = address.connect(io, .{
+        .mode = .stream,
+    }) catch return false;
+
+    defer stream.close(io);
+
+    return true;
+}
